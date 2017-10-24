91bf5551ef0ca6e9ddb3e0bbbd264d4722366113
tinkle <496047829@qq.com>
2017-10-24 09:58:01 +0800

    1024

---
+++ Array/605. Can Place Flowers.py
@@ -0,0 +1,39 @@
+# - * - coding:utf8 - * - -
+###########################################
+# Author: Tinkle
+# E-mail: shutingnjupt@gmail.com
+# Name: Can Place Flowers.py
+# Creation Time: 2017/10/24
+###########################################
+'''
+Suppose you have a long flowerbed in which some of the plots are planted and some are not. However, flowers cannot be planted in adjacent plots - they would compete for water and both would die.
+
+Given a flowerbed (represented as an array containing 0 and 1, where 0 means empty and 1 means not empty), and a number n, return if n new flowers can be planted in it without violating the no-adjacent-flowers rule.
+
+Example 1:
+Input: flowerbed = [1,0,0,0,1], n = 1
+Output: True
+Example 2:
+Input: flowerbed = [1,0,0,0,1], n = 2
+Output: False
+Note:
+The input array won't violate no-adjacent-flowers rule.
+The input array size is in the range of [1, 20000].
+n is a non-negative integer which won't exceed the input array size.
+'''
+class Solution(object):
+    def canPlaceFlowers(self, flowerbed, n):
+        """
+        :type flowerbed: List[int]
+        :type n: int
+        :rtype: bool
+        """
+        flowerbed = [0] + flowerbed + [0]
+        size = len(flowerbed)
+        for i in range(1,size-1):
+            if n==0:
+                return True
+            if flowerbed[i]==0 and flowerbed[i-1]==0 and flowerbed[i+1]==0:
+                n-=1
+                flowerbed[i]=1
+        return n==0
